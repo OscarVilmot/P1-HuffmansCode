5e17f6d85c176190d552ec5782112fafbabaffed
Oscar <oscar.vilmot@gmail.com>
2020-10-21 19:06:00 +0200

    Added 2 importants C sources

---
+++ HuffmanCode.c
@@ -0,0 +1,24 @@
+/**
+ * \file HuffmanCode.c
+ * \brief The main source where the main function takes place
+ * \date 21 oct. 2020
+ * \author Kilian.S, TommyLee.A, Joel.G, Jules.R, Oscar.V
+ * \version 0.1
+ *
+ * Compression program using Huffman Algorith, with trees and queues
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * \brief Program entry
+ *
+ * \return EXIT_SUCCESS - Normal stop from the program
+ */
+int main(){
+	printf("test");
+
+	return EXIT_SUCCESS;
+}
---
+++ treesActions.c
@@ -0,0 +1,19 @@
+/**
+ * \file treesActions.c
+ * \brief Source where all tree-related function take places
+ * \date 21 oct. 2020
+ * \author Kilian.S, TommyLee.A, Joel.G, Jules.R, Oscar.V
+ * \version 0.1
+ *
+ * Compression program using Huffman Algorith, with trees and queues
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * \brief Create a Node
+ *
+ * \return EXIT_SUCCESS - Normal stop from the program
+ */
+
