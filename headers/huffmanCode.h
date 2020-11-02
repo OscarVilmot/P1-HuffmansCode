03229305471b1f197efea0b00074031852cc4bcc
Kylian Sauvee <kylian@sauvee.com>
2020-11-02 21:54:20 +0100

    add structures OccurrenceLetter and ElementSimplyLinkedList

--- headers/huffmanCode.h
+++ headers/huffmanCode.h
@@ -5,3 +5,17 @@
  *      Author: ksauvee
  */
 
+#ifndef HUFFMAN_CODE
+#define HUFFMAN_CODE
+
+typedef struct OccurrenceLetter{
+    char letter;
+    int occurrence;
+}OccurrenceLetter;
+
+typedef struct ElementSimplyLinkedList {
+    OccurrenceLetter data;
+    struct ElementSimplyLinkedList* next;
+}ElementSimplyLinkedList;
+
+#endif
