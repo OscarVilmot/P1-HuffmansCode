b8a3be7ca0281c167bfd22b8838687bc792b7500
Oscar <oscar.vilmot@gmail.com>
2020-11-02 19:19:15 +0100

    Ajout actions sur les fichiers

---
+++ headers/fileActions.h
@@ -0,0 +1,16 @@
+/*
+ * fileActions.h
+ *
+ *  Created on: 2 nov. 2020
+ *      Author: pasarco
+ */
+
+#ifndef FILEACTIONS_H_
+#define FILEACTIONS_H_
+
+int size_text(FILE* file);
+char* read_txt();
+void write_txt(char* binary);
+
+
+#endif /* FILEACTIONS_H_ */
---
+++ sources/fileActions.c
@@ -0,0 +1,47 @@
+/**
+ * \file treesActions.c
+ * \brief Source where all tree-related function take places
+ * \date 21 oct. 2020
+ * \author Kylian.S, Tommy Lee.A, Joel.G, Jules.R, Oscar.V
+ * \version 0.1
+ *
+ * Compression program using Huffman Algorith, with trees and queues
+ *
+ */
+
+#include "../headers/structures.h"
+
+int size_text(FILE* file){
+    int size = 0;
+    char c;
+    do{
+        c = fgetc(file);
+        if (c != EOF)	size++;
+    }while(c != EOF);
+    rewind(file);
+    return size;
+}
+
+char* read_txt(){
+    FILE* file = NULL;
+    char* string;
+
+    file = fopen("../text.txt", "r");
+
+    if (file != NULL){
+        string = malloc(sizeof(char)*size_text(file));
+        fread(string, sizeof(char), size_text(file), file);
+        fclose(file);
+    }
+    printf("%s", string);
+    return string;
+}
+
+void write_txt(char* binary){
+    FILE* file = NULL;
+    file = fopen("../binary.txt", "w+");
+    if (file != NULL){
+        fwrite(binary, sizeof(char), size_text(file), file);
+        fclose(file);
+    }
+}
