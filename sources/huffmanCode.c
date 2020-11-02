1d4eba41af882666fb37273d8d5063927d8f2418
Kylian Sauvee <kylian@sauvee.com>
2020-11-02 19:36:51 +0100

    rename huffman_code.c to huffmanCode.c

---
+++ sources/huffmanCode.c
@@ -0,0 +1,12 @@
+/**
+ * \file huffmanCode.c
+ * \brief Source where all tree-related function take places
+ * \date 21 oct. 2020
+ * \author Kylian.S, Tommy Lee.A, Joel.G, Jules.R, Oscar.V
+ * \version 0.1
+ *
+ * Compression program using Huffman Algorith, with trees and queues
+ *
+ */
+
+#include "../headers/structures.h"
