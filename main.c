33cfb154ef0b8a62f7e1ed0ad73258e156e0fd31
Kylian Sauvee <kylian@sauvee.com>
2020-11-16 22:43:42 +0100

    refactor module text2bin

--- main.c
+++ main.c
@@ -9,35 +9,13 @@
  *
  */
 
-#include "./UseHuffmanCode/includes/treesActions.h"
-#include "./IHMHuffmanCode/includes/fileActions.h"
-#include "./IHMHuffmanCode/includes/text2bin.h"
-
-static void printTreePrefixe(Node* a){
-	if(a != NULL){
-		printf("%d\n", a->data);
-		printTreePrefixe(a->left);
-		printTreePrefixe(a->right);
-	}
-}
-
 /**
  * \brief Program entry
  *
  * \return EXIT_SUCCESS - Normal stop from the program
  */
-int main(){
-	/*
-	char* a = "Alice was beginning to get very tired of \n sitting by her sister on the bank, and of having nothing to do";
-	printf("%s\n %s\n", a, text2bin(a));
-	*/
-	/*
-	char a = ' ';
-	printf("%c, %d, %s", a, (int)a, char2bin(a));
-	*/
-	char* a;
-	a = read_txt();
-	printf("%s, %s", a, text2bin(a));
+#include <stdlib.h>
 
+int main() {
 	return EXIT_SUCCESS;
 }
