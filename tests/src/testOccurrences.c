de656fab9d5ea1caba09655740a10e1b2d884dc2
Kylian Sauvee <kylian@sauvee.com>
2020-12-02 22:32:57 +0100

    add function testFindListOccurrenceLettersInNull

--- tests/src/testOccurrences.c
+++ tests/src/testOccurrences.c
@@ -8,7 +8,22 @@
  * Compression program using Huffman Algorith, with trees and queues
  *
  */
+#include <stdio.h>
+#include "../../UseHuffmanCode/includes/occurrence.h"
+
+void testFindListOccurrenceLettersNull(ElementOccurrenceLetter* occurrences) {
+    findOccurrenceLettersInText(&occurrences, NULL);
+
+    if (occurrences == NULL) {
+        printf("Test 1 : SUCCESS");
+    } else {
+        printf("Test 1 : FAILURE");
+    }
+    printf("\n");
+}
 
 int main() {
+    ElementOccurrenceLetter* listOccurrences = NULL;
+    testFindListOccurrenceLettersNull(listOccurrences);
     return 0;
 }
