e9ed47b23131708afeffdab7c076e0a47394c023
Kylian Sauvee <kylian@sauvee.com>
2020-12-02 22:22:42 +0100

    add file testOccurrences.c

---
+++ tests/src/testOccurrences.c
@@ -0,0 +1,14 @@
+/**
+ * \file testOccurrences.c
+ * \brief Source where all occurrences-related tests take places
+ * \date 2 dec. 2020
+ * \author Kylian.S, Tommy Lee.A, Joel.G, Jules.R, Oscar.V
+ * \version 0.1
+ *
+ * Compression program using Huffman Algorith, with trees and queues
+ *
+ */
+
+int main() {
+    return 0;
+}
