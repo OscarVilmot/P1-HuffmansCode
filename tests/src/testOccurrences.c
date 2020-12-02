db84160aaf634305eab42eac64da590acd22d026
Kylian Sauvee <kylian@sauvee.com>
2020-12-02 23:03:29 +0100

    add function testFindListOccurrenceLettersInWord

--- tests/src/testOccurrences.c
+++ tests/src/testOccurrences.c
@@ -11,33 +11,47 @@
 #include <stdio.h>
 #include "../../UseHuffmanCode/includes/occurrence.h"
 
-void testFindListOccurrenceLettersNull(ElementOccurrenceLetter* occurrences) {
+void testFindListOccurrenceLettersNull() {
+    ElementOccurrenceLetter* occurrences = NULL;
     findOccurrenceLettersInText(&occurrences, NULL);
 
     if (occurrences == NULL) {
-        printf("Test 1 : SUCCESS");
+        printf("TEST 1 : SUCCESS");
     } else {
-        printf("Test 1 : FAILURE");
+        printf("TEST 1 : FAILURE");
     }
     printf("\n");
 }
 
-
-void testFindListOccurrenceLettersInCharacter(ElementOccurrenceLetter* occurrences) {
+void testFindListOccurrenceLettersInCharacter() {
     char* text = "a";
+    ElementOccurrenceLetter* occurrences = NULL;
     findOccurrenceLettersInText(&occurrences, text);
 
     if (occurrences->data->letter == 'a' && occurrences->data->occurrence == 1) {
-        printf("Test 2 : SUCCESS");
+        printf("TEST 2 : SUCCESS");
     } else {
-        printf("Test 2 : FAILURE");
+        printf("TEST 2 : FAILURE");
+    }
+    printf("\n");
+}
+
+void testFindListOccurrenceLettersInWord() {
+    char* text = "word";
+    ElementOccurrenceLetter* occurrences = NULL;
+    findOccurrenceLettersInText(&occurrences, text);
+
+    if (findSizeOccurrences(occurrences) == 4) {
+        printf("TEST 3 : SUCCESS");
+    } else {
+        printf("TEST 3 : FAILURE");
     }
     printf("\n");
 }
 
 int main() {
-    ElementOccurrenceLetter* listOccurrences = NULL;
-    testFindListOccurrenceLettersNull(listOccurrences);
-    testFindListOccurrenceLettersInCharacter(listOccurrences);
+    testFindListOccurrenceLettersNull();
+    testFindListOccurrenceLettersInCharacter();
+    testFindListOccurrenceLettersInWord();
     return 0;
 }
